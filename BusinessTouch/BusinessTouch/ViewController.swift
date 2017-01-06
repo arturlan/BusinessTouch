8d5713245a5676718b26ac08d3135bb169d3012f
Artur Lane <arturlan.us@gmail.com>
2017-01-06 16:34:31 -0500

    first commit

---
+++ BusinessTouch/BusinessTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessTouch
+//
+//  Created by Artur Lan on 1/6/17.
+//  Copyright © 2017 Artur Lan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
